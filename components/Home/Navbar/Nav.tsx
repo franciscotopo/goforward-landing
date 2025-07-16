dafb41898f4f3069adebe59e4b313c10f68bdaa5
Francisco Topolansky <franciscotopolansky@gmail.com>
2025-07-16 16:21:15 -0300

    Scroll Navbar Animation Logo Version 1

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { navLinks } from "@/constant/constant";
 import { HiBars3BottomRight } from "react-icons/hi2";
 
 type Props = {
-  openNav: () => void
-}
+  openNav: () => void;
+};
 
-const Nav = ({openNav}: Props ) => {
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
   return (
-    <div className="bg-black transition-all duration-200 h-[16vh] z-[1000] fixed w-full">
+    <div
+      className={`
+        ${
+          navBg ? "bg-black shadow-md" : "fixed"
+        } transition-all duration-200 h-[16vh] z-[1000] fixed w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
         {/* LOGO */}
         <div className="flex items-center h-full">
@@ -41,10 +59,13 @@ const Nav = ({openNav}: Props ) => {
             );
           })}
         </div>
-        
+
         {/* Burger Menu */}
         <div className="flex items-center space-x-4">
-        <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer text-white lg:hidden" /> 
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-white lg:hidden"
+          />
         </div>
       </div>
     </div>
