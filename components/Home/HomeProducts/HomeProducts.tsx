c574f139cc52f93d7252d9fec7e06a045078859a
Francisco Topolansky <franciscotopolansky@gmail.com>
2025-07-17 01:00:26 -0300

    AnimatedTitle Refactor

--- components/Home/HomeProducts/HomeProducts.tsx
+++ components/Home/HomeProducts/HomeProducts.tsx
@@ -1,25 +1,15 @@
 "use client";
 
-import React, { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import React from "react";
+import AnimatedTitle from "../../../hooks/AnimatedTitle";
+
 
 const HomeProducts = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, {
-    margin: "-100px 0px -40%",
-  });
 
   return (
-    <section id="products" ref={ref} className="scroll-mt-30">
+    <section id="products" className="scroll-mt-30">
       <div className="relative w-full h-[120vh] sm:h-[100vh] bg-orange-400">
-        <motion.h1
-          initial={{ opacity: 0, x: -50 }}
-          animate={isInView ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute top-10 left-10 text-white text-6xl md:text-7xl font-anton uppercase tracking-[0.5rem]"
-        >
-          Products
-        </motion.h1>
+        <AnimatedTitle text="Our Products" textColor="text-white"/>
       </div>
     </section>
   );
