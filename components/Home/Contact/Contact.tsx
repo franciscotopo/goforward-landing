d0fc5965099869be952328f819b13b301ea7a353
Francisco Topolansky <franciscotopolansky@gmail.com>
2025-07-25 19:05:05 -0300

    Contact Us Section

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className="relative w-full bg-gray-100 py-16 px-4 sm:px-10 lg:px-20">
         <AnimatedTitle text="Contact Us" textColor="text-black" />
 
-        <div className="mt-25 grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <div className="mt-35 grid grid-cols-1 lg:grid-cols-2 gap-10">
           {/* Left: Contact Info */}
           <div className="space-y-8 mt-5">
             <div>
@@ -98,7 +98,7 @@ const Contact = () => {
             </button>
             {state.succeeded && (
               <div className="mb-4 text-orange-400 font-bold">
-                <p>Message received! We'll be in touch.</p>
+                <p>Message received! We`ll keep in touch.</p>
               </div>
             )}
           </form>
