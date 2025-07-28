1785b80bb5ec60bd048b0e21e16a2f9a513bdd57
Francisco Topolansky <franciscotopolansky@gmail.com>
2025-07-28 20:25:06 -0300

    Solving Grid

--- components/Home/HomeProducts/HomeProducts.tsx
+++ components/Home/HomeProducts/HomeProducts.tsx
@@ -8,27 +8,37 @@ const HomeProducts = () => {
   return (
     <section id="products" className="scroll-mt-30">
 
-      <div className="w-full bg-orange-400 px-4 py-4 flex flex-col gap-y-2">
+      <div className="w-full bg-orange-400 px-4 py-10 flex flex-col gap-y-2 ">
 
         <div>
-          <div className="bg-red-500 rounded-lg min-h-[50px]"></div>
+          <div className="bg-red-500 rounded-lg min-h-[100px]"></div>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-x-2 gap-y-3">
           
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
-          <div className="bg-blue-500 rounded-lg min-h-[50px]"></div>
+          <div className="bg-blue-500 rounded-lg min-h-14 max-w-100">
+            <img 
+              src="images/rim1.jpeg" 
+              alt="Rim Image" 
+              className="w-auto h-auto rounded-xl cursor-pointer"
+            />
+          </div>
+          
+          <div className="bg-blue-500 rounded-lg min-h-14 max-w-100">
+            <img 
+              src="images/rim3.jpeg" 
+              alt="Combo Image" 
+              className="w-auto h-auto rounded-xl cursor-pointer"
+            />
+          </div>
+          
+          <div className="bg-blue-500 rounded-lg min-h-14 max-w-100">
+            <img 
+              src="images/rim4.jpeg" 
+              alt="Combo Image" 
+              className="w-auto h-auto rounded-xl cursor-pointer"
+            />
+          </div>
           
         </div>
 
